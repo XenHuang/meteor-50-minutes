86339d683baac8069fb46ab3907de089491e8c38
Xen <zhenfeng@hawaii.edu>
2016-10-08 21:02:17 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,17 @@
+/** E
+ * Created by X on 2016/10/8.
+ */
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
